63dc3c487e24c00a13acb503c007ed3bcec5c57b
שמואל <shmooel123@gmail.com>
2025-10-25 21:45:31 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program0814.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StageK0
+{
+
+    partial class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Welcome0814();
+            Welcome8695();
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+
+        }
+
+        static partial void Welcome8695();
+        private static  void Welcome0814()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} welcom to my first console appliction", name);
+        }
+    }
+
+}
