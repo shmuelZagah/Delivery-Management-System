6a77159a9c57c5c8e6cd01475779150b91bf572e
Daniel Bar <dabar@g.jct.ac.il>
2025-10-25 22:25:20 +0300

    adding im also here

---
+++ Stage0/Program8695.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    internal class Program8695
+    {
+        static void Welcome8695()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
