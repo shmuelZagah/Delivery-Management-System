e9ea2253d736aede9a956d9c95f213f2a92a0d12
שמואל <shmooel123@gmail.com>
2025-10-30 17:11:30 +0200

    add IConfig to DalApi

---
+++ DalFacade/DalApi/IConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace DalApi;
+using DO;
+
+public interface IConfig
+{
+    void Create(Config item);        //Creates new entity object in DAL
+    Config? Read(int id);            //Reads entity object by its ID
+    void Update(Config item);        //Updates entity object
+    void Delete(int id);             //Deletes an object by its Id
+    void DeleteAll();                //Delete all entity objects
+    List<Config> ReadAll();          //stage 1 only, Reads all entity objects
+}
