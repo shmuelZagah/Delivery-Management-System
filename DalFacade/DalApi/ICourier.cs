81a2ef6c882240f8a54a8f4a1f417872a530d802
שמואל <shmooel123@gmail.com>
2025-10-30 16:59:46 +0200

    step5 crated a DalApi interface

---
+++ DalFacade/DalApi/ICourier.cs
@@ -0,0 +1,11 @@
+﻿namespace DalApi;
+using DO;
+public interface ICourier
+{
+    void Create(Courier item);     //Creates new entity object in DAL
+    Courier? Read(int id);         //Reads entity object by its ID
+    List<Courier> ReadAll();       //stage 1 only, Reads all entity objects
+    void Update(Courier item);     //Updates entity object
+    void Delete(int id);            //Deletes an object by is Id
+    void DeleteAll();               //Delete all entity objects
+}
