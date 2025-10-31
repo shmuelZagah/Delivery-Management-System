ce30d9c2b93ce946858f40af594129e7640170d7
שמואל <shmooel123@gmail.com>
2025-10-31 11:46:35 +0200

    step 6

---
+++ DalList/DataSource.cs
@@ -0,0 +1,8 @@
+﻿namespace Dal;
+using DO;
+internal static class DataSource
+{
+    internal static List<DO.Courier> couriers = new();
+    internal static List<DO.Delivery> deliveries = new();
+    internal static List<DO.Order> orders = new();
+}
