6655c023c1f34d622e04b0452f8409fb0de4436e
Daniel Bar <dabar@g.jct.ac.il>
2025-11-01 20:39:26 +0200

    start of DalTest Stage10

---
+++ DalTest/Intialization.cs
@@ -0,0 +1,37 @@
+﻿namespace DalTest;
+using DalApi;
+using DO;
+
+public static class Intialization
+{
+
+    private static IConfig? s_DalConfig;
+    private static ICourier? s_DalCourier;
+    private static IDelivery? s_DalDelivery;
+    private static IOrder? s_DalOrder;
+
+    private static readonly Random s_rand = new();
+
+    private  static IConfig? createConfig()
+    {
+
+
+    }
+
+    private static ICourier? createCourier()
+    {
+
+    }
+
+    private static IDelivery? createDelivery()
+    {
+
+    }
+
+    private static IOrder? createOrder()
+    {
+
+    }
+
+
+}
