8de5b649d3a4c89b9c263da4c13068d63ecfb44b
Daniel Bar <dabar@g.jct.ac.il>
2025-11-17 20:40:13 +0200

    4.7 final

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,24 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class DalXml: IDal
+    {
+        public ICourier Courier => new CourierImplementation();
+        public IOrder Order => new OrderImplementation();
+        public IDelivery Delivery => new DeliveryImplementation();
+        public IConfig Config => new ConfigImplementation();
+        public void ResetDB()
+        {
+            Delivery.DeleteAll();
+            Order.DeleteAll();
+            Courier.DeleteAll();
+            Config.Reset();
+        }
+    }
+}
