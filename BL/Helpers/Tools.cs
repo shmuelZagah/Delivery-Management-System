d82ded527ef50e7fb821ebb6856afcece14debb7
Daniel Bar <dabar@g.jct.ac.il>
2025-11-29 23:44:42 +0200

    stage 4.7

---
+++ BL/Helpers/Tools.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    internal static class Tools
+    {
+        public static string ToStringProperty<T>(this T t)
+        {
+            if (t == null)
+                return "";
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            List<string> lines = new();
+
+            foreach (var prop in properties)
+            {
+                object? value = prop.GetValue(t);
+
+                // הדפסה יפה לרשימות
+                if (value is System.Collections.IEnumerable list &&
+                    value is not string)
+                {
+                    List<string> items = new();
+                    foreach (var item in list)
+                        items.Add(item?.ToString() ?? "null");
+
+                    lines.Add($"{prop.Name}: [ {string.Join(", ", items)} ]");
+                }
+                else
+                {
+                    lines.Add($"{prop.Name}: {value}");
+                }
+            }
+
+            return string.Join("\n", lines);
+        }
+    }
+}
