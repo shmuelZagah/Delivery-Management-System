f3128f3d4f46c45bc00836c1522b6557fae05452
Daniel Bar <dabar@g.jct.ac.il>
2025-11-29 23:21:47 +0200

    did everything until 4.7

---
+++ BL/BO/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Config
+    {
+        // --- COMPANY DETAILS ---
+        public string? CompanyAddress { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public double? MaxAirRange { get; set; }
+
+        // --- AVERAGE SPEEDS (optional for ETA calculations on UI) ---
+        public double AvgCarSpeed { get; set; }
+        public double AvgMotocyclerSpeed { get; set; }
+        public double AvgBicycleSpeed { get; set; }
+        public double AvgWalkingSpeed { get; set; }
+
+    }
+}
