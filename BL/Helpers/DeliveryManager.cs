775325d4784951ac64196673cec73564a0952a55
Daniel Bar <dabar@g.jct.ac.il>
2025-12-01 16:39:12 +0200

    stage 4.7

---
+++ BL/Helpers/DeliveryManager.cs
@@ -0,0 +1,45 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    internal class DeliveryManager
+    {
+        private static IDal s_dal = Factory.Get;
+
+        internal static DO.Delivery? GetDelivery(int id)
+        {
+            DO.Delivery doDelivery;
+
+            try
+            {
+                doDelivery = s_dal.Delivery.Read(id)
+                    ?? throw new BO.BlDoesNotExistException($"Delivery with ID={id} does not exist");
+            }
+            catch (DO.DalDoesNotExistException ex)
+            {
+                throw new BO.BlDoesNotExistException($"Delivery with ID={id} does not exist", ex);
+            }
+
+            return new DO.Delivery()
+            {
+                Id = doDelivery.Id,
+                OrderId = doDelivery.OrderId,
+                CourierId = doDelivery.CourierId,
+
+                DeliveryType = (DO.ShipmentType)doDelivery.DeliveryType,
+
+                StartTime = doDelivery.StartTime,
+                DistanceKm = doDelivery.DistanceKm,
+
+                DeliveryEndType = (DO.DeliveryEndType?)doDelivery.DeliveryEndType,
+                EndTime = doDelivery.EndTime
+            };
+        }
+
+    }
+}
---
+++ BL/Helpers/OrderManager.cs
@@ -0,0 +1,16 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    internal class OrderManager
+    {
+        private static IDal s_dal = Factory.Get; 
+
+    }
+}
